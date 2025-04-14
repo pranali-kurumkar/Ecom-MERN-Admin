bd0fb42c3d94e8b056ac6c413636267316832fc4
pranali-kurumkar <pranalikurumkar07@gmail.com>
2025-04-14 22:06:32 +0530

    Initial commit for admin

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from './Components/Navbar/Navbar'
+import Sidebar from './Components/Sidebar/Sidebar'
+import Admin from './Pages/Admin/Admin'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Admin/>
+      
+    </div>
+  )
+}
+
+export default App
